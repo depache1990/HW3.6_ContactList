0398245bfac60902ac7947bebf263a239a988182
parkurru@mail.ru <parkurru@mail.ru>
2021-11-12 11:28:19 +0200

    Initial Commit

---
+++ HW3.6_ContactList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.6_ContactList
+//
+//  Created by Anton Duplin on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.6_ContactList/HW3_6_ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_6_ContactListApp.swift
+//  HW3.6_ContactList
+//
+//  Created by Anton Duplin on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HW3_6_ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
